8e54f5005e0a5e45950f1af4aca4e2ae50d6c01d
Alexander Koskovich <akoskovich@pm.me>
2024-09-03 23:13:29 -0400

    zumapro: Add relevant packages/properties for /product & /system[_ext]
    Change-Id: I4f892c908ec99c9068873718acec107a383196ab

---
+++ device-system.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.camerax.extensions.enabled=true
+
+# eSIM
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# GMS
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.clientidbase=android-google
+
+# Graphics
+PRODUCT_PACKAGES += \
+    ANGLE
+
+# Hotword
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.hotword.detection_service_required=true
+
+# OPA
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
