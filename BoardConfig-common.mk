GuidixX/zuma

Set BOARD_LIBACRYL_G2D_HDR_PLUGIN for soong version
Set the full module path of libacryl_hdr_plugin for libacryl conversion
to soong.

Bug: 370414334
Test: m libacryl
Change-Id: I253ce1254450385e83ecaa75acbee5e70df3a930

BoardConfig-common.mk
@@ -274,6 +274,7 @@ BOARD_USES_EXYNOS_AFBC_FEATURE := true
 
 BOARD_LIBACRYL_DEFAULT_COMPOSITOR := fimg2d_zuma
 BOARD_LIBACRYL_G2D_HDR_PLUGIN := libacryl_hdr_plugin
+$(call soong_config_set,acryl,libacryl_g2d_hdr_plugin,//hardware/google/graphics/gs201/libacryl_plugins:libacryl_hdr_plugin)
 
 # HWCServices
 BOARD_USES_HWC_SERVICES := true

ZVNexus/android_device_google_zumapro

BoardConfig-common.mk
@@ -280,6 +280,7 @@ BOARD_USES_EXYNOS_AFBC_FEATURE := true
 
 BOARD_LIBACRYL_DEFAULT_COMPOSITOR := fimg2d_zuma
 BOARD_LIBACRYL_G2D_HDR_PLUGIN := libacryl_hdr_plugin
+$(call soong_config_set,acryl,libacryl_g2d_hdr_plugin,//hardware/google/graphics/zuma/libacryl_plugins:libacryl_hdr_plugin)
 
 # HWCServices
 BOARD_USES_HWC_SERVICES := true